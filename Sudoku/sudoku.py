716a5f7512e4860424b9236a6b306211c43fadb2
aryangirii <157490203+aryangirii@users.noreply.github.com>
2025-04-24 22:36:50 +0530

    Update sudoku.py

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -2,29 +2,43 @@ import random
 import copy
 
 def generate_full_board():
-    board = [[0] * 9 for _ in range(9)]
-    fill_board(board)
+    board = [[0 for _ in range(9)] for _ in range(9)]
+
+    def is_valid(board, row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
+                return False
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == num:
+                    return False
+        return True
+
+    def solve(board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    nums = list(range(1, 10))
+                    random.shuffle(nums)
+                    for num in nums:
+                        if is_valid(board, row, col, num):
+                            board[row][col] = num
+                            if solve(board):
+                                return True
+                            board[row][col] = 0
+                    return False
+        return True
+
+    solve(board)
     return board
 
-def fill_board(board):
-    num_list = list(range(1, 10))
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == 0:
-                random.shuffle(num_list)
-                for num in num_list:
-                    if is_valid(board, num, (i, j)):
-                        board[i][j] = num
-                        if fill_board(board):
-                            return True
-                        board[i][j] = 0
-                return False
-    return True
-
-def generate_puzzle():
+def generate_puzzle(difficulty="easy"):
     board = generate_full_board()
     puzzle = copy.deepcopy(board)
-    attempts = 40
+    removals = {"easy": 30, "medium": 40, "hard": 50}
+    attempts = removals.get(difficulty, 30)
+
     while attempts > 0:
         row, col = random.randint(0, 8), random.randint(0, 8)
         while puzzle[row][col] == 0:
@@ -33,29 +47,30 @@ def generate_puzzle():
         attempts -= 1
     return puzzle
 
-def is_valid(board, num, pos):
-    row, col = pos
-    for i in range(9):
-        if board[row][i] == num and i != col:
-            return False
-        if board[i][col] == num and i != row:
-            return False
-    box_x, box_y = col // 3, row // 3
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if board[i][j] == num and (i, j) != pos:
+def solve_board(board):
+    def is_valid(board, row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
                 return False
-    return True
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == num:
+                    return False
+        return True
 
-def solve_sudoku(board):
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                for num in range(1, 10):
-                    if is_valid(board, num, (row, col)):
-                        board[row][col] = num
-                        if solve_sudoku(board):
-                            return board
-                        board[row][col] = 0
-                return None
+    def solve(board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for num in range(1, 10):
+                        if is_valid(board, row, col, num):
+                            board[row][col] = num
+                            if solve(board):
+                                return True
+                            board[row][col] = 0
+                    return False
+        return True
+
+    solve(board)
     return board
